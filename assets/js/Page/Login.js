7478c2c01ee42a0f7a73a353455811bdecf7282c
mounkaila <mounkaila144@gmail.com>
2022-04-14 23:32:38 +0100

    correction 24

--- assets/js/Page/Login.js
+++ assets/js/Page/Login.js
@@ -68,7 +68,7 @@ export default function Login() {
                         authState: res.config.data
                     }))
                     {
-                        navigate('')
+                        navigate('/react/menu')
                     }
                     else {
 
--- assets/js/Page/Menu.js
+++ assets/js/Page/Menu.js
@@ -5,14 +5,16 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import {useEffect, useState} from "react";
 import axios from "axios";
-import {Backdrop, CircularProgress} from "@mui/material";
+import {Backdrop, CircularProgress, ListItem, ListItemIcon, ListItemText} from "@mui/material";
 import PubCard from "../components/card/PubCard";
 import {orange, pink, red} from "@mui/material/colors";
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
-import DeliveryDiningIcon from '@mui/icons-material/DeliveryDining';
+import StarIcon from '@mui/icons-material/Star';
 import CardContent from "@mui/material/CardContent";
 import Card from "@mui/material/Card";
 import IconButton from "@mui/material/IconButton";
+import ListContext from "@mui/material/List/ListContext";
+import List from "@mui/material/List";
 
 const Item = styled(Paper)(({theme}) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -34,6 +36,10 @@ export default function Menu() {
                     <Grid container spacing={{xs: 1, md: 1}} columns={{xs: 12, sm: 12, md: 12}} alignContent={"center"}
                           justifyContent={'center'}>
                         <Grid item xs={10} sm={10} md={5}>
+                            <Box component="div"
+                                 sx={{overflow: 'auto', fontSize: 15, marginBottom: 2, color: 'white'}}>
+                                Bienvenue sur Allcine227 ici sera rediger la presentation du Boutique
+                            </Box>
                             <IconButton aria-label="cart">
                                 <MonetizationOnIcon sx={{fontSize: 50, marginTop: -2, color: orange[900]}}/>
                                 <Box component="div"
@@ -47,34 +53,127 @@ export default function Menu() {
                     <Card>
                         <CardContent
                             sx={{boxShadow: 3, bgcolor: pink[500], marginBottom: 2, justifyContent: 'center'}}>
-                            👉🏾👉🏾 series<br/>
-                            1 saison a 500f
-                            2 saisons a 1000f
-                            3 saisons a 1500f
-                            🥳 a partir de 2000f,beneficiez d une saison bonus de votre choix.<br/>
-                        </CardContent>
-                    </Card>
-                    <Card>
-                        <CardContent
-                            sx={{boxShadow: 3, bgcolor: pink[500], marginBottom: 2, justifyContent: 'center'}}>
-                            👉🏾👉🏾 films/animés<br/>
-                            1 film a 200f ou 3 films a 500f.<br/>
-                        </CardContent>
-                    </Card>
-                    <Card>
-                        <CardContent
-                            sx={{boxShadow: 3, bgcolor: pink[500], marginBottom: 2, justifyContent: 'center'}}>
-                            👉🏾👉🏾 mangas<br/>
-                            40 épisodes a 500f<br/>
-                        </CardContent>
-                    </Card>
+                            <Box component="div"
+                                 sx={{
+                                     overflow: 'auto',
+                                     fontSize: 25,
+                                     fontWeight: "bold",
+                                     marginLeft: 5,
+                                     color: 'black'
+                                 }}>
+                                SERIES
+                            </Box>
+                            <List>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <StarIcon sx={{color: "white"}}/>
+                                    </ListItemIcon>
+                                    <Box component="div"
+                                         sx={{overflow: 'auto', fontSize: 17, color: 'black'}}>
+                                        1 saison à 500f
+                                    </Box>
+                                </ListItem>
 
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <StarIcon sx={{color: "white"}}/>
+                                    </ListItemIcon>
+                                    <Box component="div"
+                                         sx={{overflow: 'auto', fontSize: 17, color: 'black'}}>
+                                        2 saisons à 1000f
+                                    </Box>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <StarIcon sx={{color: "white"}}/>
+                                    </ListItemIcon>
+                                    <Box component="div"
+                                         sx={{overflow: 'auto', fontSize: 17, color: 'black'}}>
+                                        3 saisons à 1500f
+                                    </Box>
+                                </ListItem>
+                            </List>
+
+                            <Box component="div"
+                                 sx={{overflow: 'auto', fontSize: 19, color: 'black'}}>
+                                A partir de 2000f,beneficiez d une saison bonus de votre choix
+                            </Box>
+                        </CardContent>
+                    </Card>
                     <Card>
                         <CardContent
                             sx={{boxShadow: 3, bgcolor: pink[500], marginBottom: 2, justifyContent: 'center'}}>
-                            <Box component="div" sx={{overflow: 'auto', fontSize: 22, marginBottom: 2, marginTop: 2}}>
-                                🥳Si vous payez une clé USB ou carte mémoire,vous bénéficierez d'un remplissage
-                                gratuit(De votre choix).
+                            <Box component="div"
+                                 sx={{
+                                     overflow: 'auto',
+                                     fontSize: 25,
+                                     fontWeight: "bold",
+                                     marginLeft: 5,
+                                     color: 'black'
+                                 }}>
+                                FILM ET ANIME
+                            </Box>
+                            <List>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <StarIcon sx={{color: "white"}}/>
+                                    </ListItemIcon>
+                                    <Box component="div"
+                                         sx={{overflow: 'auto', fontSize: 17, color: 'black'}}>
+                                        1 film a 200f
+                                    </Box>
+                                </ListItem>
+
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <StarIcon sx={{color: "white"}}/>
+                                    </ListItemIcon>
+                                    <Box component="div"
+                                         sx={{overflow: 'auto', fontSize: 17, color: 'black'}}>
+                                        3 films a 500f
+                                    </Box>
+                                </ListItem>
+                            </List>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent
+                            sx={{boxShadow: 3, bgcolor: pink[500], marginBottom: 2, justifyContent: 'center'}}>
+                            <Box component="div"
+                                 sx={{
+                                     overflow: 'auto',
+                                     fontSize: 25,
+                                     fontWeight: "bold",
+                                     marginLeft: 5,
+                                     color: 'black'
+                                 }}>
+                                MANGA
+                            </Box>
+                            <List>
+                                <ListItem>
+                                    <ListItemIcon>
+                                        <StarIcon sx={{color: "white"}}/>
+                                    </ListItemIcon>
+                                    <Box component="div"
+                                         sx={{overflow: 'auto', fontSize: 17, color: 'black'}}>
+                                        40 épisodes a 500f
+                                    </Box>
+                                </ListItem>
+                            </List>
+                        </CardContent>
+                    </Card>
+                    <Card>
+                        <CardContent
+                            sx={{boxShadow: 3, bgcolor: pink[500], marginBottom: 2, justifyContent: 'center'}}>
+                            <Box component="div" sx={{overflow: 'auto', fontSize: 17, marginBottom: 2, marginTop: 2}}>
+                                Si vous achetez une clé USB ou une carte mémoire,<br/>
+                                vous bénéficierez d'un remplissage gratuit<br/>
+                                <Box component="div"
+                                     sx={{overflow: 'auto', fontSize: 17, color: 'white'}}>
+                                    De votre choix
+                                </Box>
+
                             </Box>
                         </CardContent>
                     </Card>
